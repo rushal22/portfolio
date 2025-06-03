ad8e1edce44fedc6b81ef6c238962a1380d5554e
numb123jhiyo <numb123jhiyo@gmail.com>
2025-06-04 05:23:25 +0545

    Added meta tag and og title

--- Rushal_maharjan_portfolio/app.js
+++ Rushal_maharjan_portfolio/app.js
@@ -1,25 +1,45 @@
-const hamburger = document.querySelector('.header .nav-bar .nav-list .hamburger');
-const mobile_menu = document.querySelector('.header .nav-bar .nav-list ul');
-const menu_item = document.querySelectorAll('.header .nav-bar .nav-list ul li a');
-const header = document.querySelector('.header.container');
+document.addEventListener("DOMContentLoaded", () => {
+  const hamburger = document.querySelector(
+    ".header .nav-bar .nav-list .hamburger"
+  );
+  const mobile_menu = document.querySelector(".header .nav-bar .nav-list ul");
+  const menu_item = document.querySelectorAll(
+    ".header .nav-bar .nav-list ul li a"
+  );
+  const header = document.querySelector(".header.container");
+  const ogTitle = document.querySelector('meta[property="og:title"]');
+  const pageTitle = document.querySelector("title");
+  const ogDescription = document.querySelector(
+    'meta[property="og:description"]'
+  );
 
-hamburger.addEventListener('click', () => {
-	hamburger.classList.toggle('active');
-	mobile_menu.classList.toggle('active');
-});
+  hamburger.addEventListener("click", () => {
+    hamburger.classList.toggle("active");
+    mobile_menu.classList.toggle("active");
+  });
 
-document.addEventListener('scroll', () => {
-	var scroll_position = window.scrollY;
-	if (scroll_position > 250) {
-		header.style.backgroundColor = '#29323c';
-	} else {
-		header.style.backgroundColor = 'transparent';
-	}
-});
+  document.addEventListener("scroll", () => {
+    var scroll_position = window.scrollY;
+    if (scroll_position > 250) {
+      header.style.backgroundColor = "#29323c";
+    } else {
+      header.style.backgroundColor = "transparent";
+    }
+  });
 
-menu_item.forEach((item) => {
-	item.addEventListener('click', () => {
-		hamburger.classList.toggle('active');
-		mobile_menu.classList.toggle('active');
-	});
+  menu_item.forEach((item) => {
+    item.addEventListener("click", () => {
+      hamburger.classList.toggle("active");
+      mobile_menu.classList.toggle("active");
+      const sectionName = item.getAttribute("data-after");
+      if (sectionName) {
+        ogTitle.setAttribute("content", `Rushal Maharjan - ${sectionName}`);
+        pageTitle.textContent = `Rushal Maharjan - ${sectionName}`;
+        ogDescription.setAttribute(
+          "content",
+          `Explore Rushal Maharjan's portfolio - ${sectionName} section`
+        );
+      }
+    });
+  });
 });
